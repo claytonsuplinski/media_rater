c2e26d0bb18cbf860b94117206f8efd66fb69564
claytonsuplinski <claytonsuplinski@gmail.com>
2018-03-29 23:59:34 -0500

    Fixed Minor UI Issues and Added More Content

--- assets/js/Config.js
+++ assets/js/Config.js
@@ -2,6 +2,6 @@ MIA = {};
 
 MIA.config = {};
 
-MIA.config.menu_options = [ "Movies", "Television", "Video Games", "Music Artists", "Songs" ];
+MIA.config.menu_options = [ "Movies", "Television", "Video Games", "Music Artists", "Songs", "Pokemon" ];
 
 MIA.config.document_title = 'Media Rater';

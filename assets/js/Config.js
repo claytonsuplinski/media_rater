c40cb5a95fcec31b83e7cef9d0b6e01f8233ce57
claytonsuplinski <claytonsuplinski@gmail.com>
2021-06-16 09:57:28 -0500

    More content. Added Characters view.

--- assets/js/Config.js
+++ assets/js/Config.js
@@ -2,6 +2,6 @@ MIA = {};
 
 MIA.config = {};
 
-MIA.config.menu_options = [ "Movies", "Television", "Video Games", "Music Artists", "Songs", "Pokemon", "Athletes", "TV Episodes", "Restaurants", "Months", "Years" ];
+MIA.config.menu_options = [ "Movies", "Television", "Video Games", "Characters", "Music Artists", "Songs", "Pokemon", "Athletes", "TV Episodes", "Restaurants", "Months", "Years" ];
 
 MIA.config.document_title = 'Media Rater';

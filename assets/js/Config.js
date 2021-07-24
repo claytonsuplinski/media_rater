2cef7cf550cbb3dd2f23ec7967de9518b35b6ee3
claytonsuplinski <claytonsuplinski@gmail.com>
2021-07-23 21:44:13 -0500

    Added sporting events. More content.

--- assets/js/Config.js
+++ assets/js/Config.js
@@ -2,7 +2,7 @@ MIA = {};
 
 MIA.config = {};
 
-MIA.config.menu_options = [ "Movies", "Television", "Video Games", "Characters", "Music Artists", "Songs", "Pokemon", "Athletes", "TV Episodes", "Restaurants", "Months", "Years" ];
+MIA.config.menu_options = [ "Movies", "Television", "Video Games", "Characters", "Music Artists", "Songs", "Pokemon", "Athletes", "Sporting Events", "TV Episodes", "Restaurants", "Months", "Years" ];
 
 MIA.config.pages_with_properties = [ "Movies" ];
 

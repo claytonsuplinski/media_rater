7f1502bf24b59d109193e34f5baecd55de11db9a
claytonsuplinski <claytonsuplinski@gmail.com>
2021-07-31 08:57:20 -0500

    More content.

--- assets/js/Config.js
+++ assets/js/Config.js
@@ -4,7 +4,7 @@ MIA.config = {};
 
 MIA.config.menu_options = [ "Movies", "Television", "Video Games", "Characters", "Music Artists", "Songs", "Pokemon", "Athletes", "Sporting Events", "TV Episodes", "Restaurants", "Months", "Years" ];
 
-MIA.config.pages_with_properties = [ "Movies" ];
+MIA.config.pages_with_properties = [ "Movies", "Characters" ];
 
 MIA.config.max_stars = {
 	Months : 12,

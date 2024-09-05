ba5451d407b4d616fe4a2683c4c5a0c2284b52f3
claytonsuplinski <claytonsuplinski@gmail.com>
2024-09-04 22:03:19 -0500

    .

--- assets/js/views/Random.js
+++ assets/js/views/Random.js
@@ -14,14 +14,13 @@ MIA.content.views.random.get_content = function( self ){
 	
 	var item = JL.functions.random_element( data );
 
-	return '<input id="random-filter" onchange="MIA.content.views.random.filter_value = this.value;" onfocus="MIA.keyboard.disable([ \'/\', \'ESCAPE\' ]);" onblur="MIA.keyboard.enable();"></input>' +
-		'<div id="random-refresh" onclick="MIA.content.draw();"><i class="fa fa-refresh"></i></div>' +
-		'<br>' +
-		'<div class="item no-highlight" style="background-image:url(' + MIA.functions.get_image( MIA.content.name, item.name ) + ');">'+
+	return '<br><div class="item no-highlight" style="background-image:url(' + MIA.functions.get_image( MIA.content.name, item.name ) + ');">'+
 			'<div class="rating">#' + item.rank + '</div>'+
 			'<div class="stars">' + item.total_rating + ' <i class="fa fa-star '+ MIA.functions.get_rank_class( item.rank ) + '"></i></div>'+
 			'<div class="name">' + item.name + (item.year ? ' (' + item.year + ')' : '') + '</div>'+
-		'</div>';
+		'</div><br>' +
+		'<input id="random-filter" onchange="MIA.content.views.random.filter_value = this.value;" onfocus="MIA.keyboard.disable([ \'/\', \'ESCAPE\' ]);" onblur="MIA.keyboard.enable();"></input>' +
+		'<div id="random-refresh" onclick="MIA.content.draw();"><i class="fa fa-refresh"></i></div>';
 };
 
 MIA.content.views.random.post_draw = function( self ){

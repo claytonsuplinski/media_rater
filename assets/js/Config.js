9b9fb104c78fd06957a91e42b18d887f00d85208
claytonsuplinski <claytonsuplinski@gmail.com>
2026-04-17 22:17:37 -0500

    .

--- assets/js/Config.js
+++ assets/js/Config.js
@@ -2,7 +2,10 @@ MIA = {};
 
 MIA.config = {};
 
-MIA.config.menu_options = [ "Movies", "Video Games", "Television", "Stadiums", "Sporting Events", "TV Episodes", "Characters", "Music Artists", "Songs", "Pokemon", "Athletes", "Restaurants", "Months", "Years" ];
+MIA.config.is_dev = !( window.location.href.includes( '.io/' ) );
+
+                        MIA.config.menu_options = [ "Movies", "Video Games", "TV Episodes", ];
+if( MIA.config.is_dev ) MIA.config.menu_options = [ "Movies", "Video Games", "TV Episodes", "Television", "Stadiums", "Sporting Events", "Characters", "Music Artists", "Songs", "Pokemon", "Athletes", "Restaurants", "Months", "Years" ];
 
 MIA.config.pages_with_properties = [ "Movies", "Characters" ];
 
